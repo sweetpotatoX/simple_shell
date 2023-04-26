9b03bbd115e7f39c196220852378f02f02c9b3ab
sweetpotatoX <abdobahaa53@gmail.com>
2023-04-25 17:32:40 -0700

    league

--- simple_shell.c
+++ simple_shell.c
@@ -1,32 +1,74 @@
 #include "shell.h"
-#define MAX_CMD_LENGTH 100
 
-int main()
+/**
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
 {
-    char cmd[MAX_CMD_LENGTH];
-    int status;
+    return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
 
-    while (1) {
-        printf("#cisfun$ ");
-        if (fgets(cmd, MAX_CMD_LENGTH, stdin) == NULL) {
-
-            break;
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+    for (int i = 0; delim[i] != '\0'; i++) {
+        if (c == delim[i]) {
+            return 1;
         }
-        cmd[strcspn(cmd, "\n")] = '\0';
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("fork failed");
-} 
-else if (pid == 0)
+    }
+    return 0;
+}
+/**
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+int is_alpha(char c) {
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) ? 1 : 0;
+}
+/**
+ * _atoi - converts a string to an integer.
+ * @s: An input string.
+ * Return: 0 if no numbers in string, converted number if not.
+ */
+int _atoi(char *s)
 {
-execl(cmd, cmd, (char*)NULL);
-perror("exec failed");
-exit(EXIT_FAILURE);
-}
-else
-{          
-wait(&status);
-}
-}
-return 0;
+    int sign = 1;
+    unsigned int total = 0;
+    char null_flag = 0;
+
+    if (s == NULL)
+        return (0);
+    while (*s)
+    {
+        if (*s == '-')
+            sign *= -1;
+        if (*s >= '0' && *s <= '9')
+        {
+            null_flag = 1;
+            total = total * 10 + (*s - '0');
+        }
+        else if (*s == '+' && null_flag == 0)
+        {
+            null_flag = 1;
+        }
+        else if (*s < '0' || *s > '9')
+        {
+            if (null_flag == 1)
+                break;
+        }
+        s++;
+    }
+    if (sign < 0)
+        total = (-1 * (total));
+    return (total);
 }
