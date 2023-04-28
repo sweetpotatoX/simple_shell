e18816cba6dffef335439191afb62aacf8315feb
sweetpotatoX <67162997+sweetpotatoX@users.noreply.github.com>
2023-04-29 00:38:56 +0200

    revoke

--- builtin1.c
+++ builtin1.c
@@ -18,22 +18,22 @@ int _myhistory(info_t *info)
  * @info: parameter struct
  * @str: the string alias
  *
- * Return: 0 on success, 1 on error
+ * Return: Always 0 on success, 1 on error
  */
 int unset_alias(info_t *info, char *str)
 {
-	char *point; /* pointer to the location of the '=' character */
-	int retu; /* return value */
+	char *p, c;
+	int ret;
 
-	point = _strchr(str, '='); /* find location of the '=' character in the string */
-	if (!point) /* if no '=' character found */
-		return (1); /* return 1 to indicate error */
-	char ch = *point; /* save the character at the location of the '=' character */
-	*point = '\0'; /* set the character at the location of the '=' to null terminator */
-	retu = delete_node_at_index(&(info->alias), /* delete the alias node */
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	c = *p;
+	*p = 0;
+	ret = delete_node_at_index(&(info->alias),
 		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
-	*point = ch; /* restore the character at the location of the '=' character */
-	return (retu); /* return the return value of delete_node_at_index() */
+	*p = c;
+	return (ret);
 }
 
 /**
@@ -45,21 +45,15 @@ int unset_alias(info_t *info, char *str)
  */
 int set_alias(info_t *info, char *str)
 {
-	char *point;
+	char *p;
 
-	/* Find the first '=' in the string */
-	point = _strchr(str, '=');
-	if (!point)
+	p = _strchr(str, '=');
+	if (!p)
 		return (1);
-
-	/* Check if the alias value is empty */
-	if (!*++point)
+	if (!*++p)
 		return (unset_alias(info, str));
 
-	/* Remove any existing alias with the same name */
 	unset_alias(info, str);
-
-	/* Add the new alias to the end of the alias linked list */
 	return (add_node_end(&(info->alias), str, 0) == NULL);
 }
 
@@ -71,30 +65,30 @@ int set_alias(info_t *info, char *str)
  */
 int print_alias(list_t *node)
 {
-	char *p = NULL, *af = NULL;
+	char *p = NULL, *a = NULL;
 
 	if (node)
 	{
 		p = _strchr(node->str, '=');
-		for (af = node->str; af <= p; af++)
-			_putchar(*af);
+		for (a = node->str; a <= p; a++)
+		_putchar(*a);
 		_putchar('\'');
-		_puts(p + 1); /*Print everything after the '=' character*/
+		_puts(p + 1);
 		_puts("'\n");
 		return (0);
 	}
 	return (1);
 }
+
 /**
  * _myalias - mimics the alias builtin (man alias)
  * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- *
- * Return: Always 0
+ *          constant function prototype.
+ *  Return: Always 0
  */
 int _myalias(info_t *info)
 {
-	int index = 0;
+	int i = 0;
 	char *p = NULL;
 	list_t *node = NULL;
 
@@ -108,13 +102,13 @@ int _myalias(info_t *info)
 		}
 		return (0);
 	}
-	for (index = 1; info->argv[index]; index++)
+	for (i = 1; info->argv[i]; i++)
 	{
-		p = _strchr(info->argv[index], '=');
+		p = _strchr(info->argv[i], '=');
 		if (p)
-			set_alias(info, info->argv[index]);
+			set_alias(info, info->argv[i]);
 		else
-			print_alias(node_starts_with(info->alias, info->argv[index], '='));
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
 	}
 
 	return (0);
