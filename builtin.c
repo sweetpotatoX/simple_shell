3e59ae3bb8138565285c34ab19514786f4ae2bcf
sweetpotatoX <67162997+sweetpotatoX@users.noreply.github.com>
2023-04-29 00:57:20 +0200

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -29,53 +29,52 @@ int _myexit(info_t *info)
 }
 
 /**
- * _mycd - Changes current working directory
- * @info: Pointer to struct of shell info
  *
- * Return: 0 on success, 1 on error
- */
+ *_mycd - Changes current working directory
+ *@info: Pointer to struct of shell info
+ *Return: 0 on success, 1 on error
+*/
 int _mycd(info_t *info)
 {
-	char *stringer, *direr, buff[1024];
-	int chdirer_retu;
-	stringer = getcwd(buff, 1024); 
-	if (!stringer)
-		_puts("TODO: >>getcwd failure emsg here<<\n");
-	/* If no argument, change to home directory */
-	if (!info->argv[1])
-	{
-		direr = _getenv(info, "HOME=");
-		if (!direr)
-			chdirer_retu = chdir((direr = _getenv(info, "PWD=")) ? direr : "/");
-		else
-			chdirer_retu = chdir(direr);
-	}
-        else if(_strcmp(info->argv[1], "-") == 0) /* If argument is "-"*/
-	{
-		if (!_getenv(info, "OLDPWD="))
-		{
-			_puts(stringer);
-			_putchar('\n');
-			return (1);
-		}
-		_puts(_getenv(info, "OLDPWD="));
-		_putchar('\n');
-		chdirer_retu = chdir((direr = _getenv(info, "OLDPWD=")) ? direr : "/");
-	}
-	else
-	chdirer_retu = chdir(info->argv[1]);
-	if (chdirer_retu == -1)
-	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]);
-		_eputchar('\n');
-	}
-	else
-	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buff, 1024));
-	}
-	return (0);
+char *stringer, *direr, buff[1024];
+int chdirer_retu;
+stringer = getcwd(buff, 1024);
+if (!stringer)
+_puts("TODO: >>getcwd failure emsg here<<\n");
+if (!info->argv[1])
+{
+direr = _getenv(info, "HOME=");
+if (!direr)
+chdirer_retu = chdir((direr = _getenv(info, "PWD=")) ? direr : "/");
+else
+chdirer_retu = chdir(direr);
+}
+else if (_strcmp(info->argv[1], "-") == 0)
+{
+if (!_getenv(info, "OLDPWD="))
+{
+_puts(stringer);
+_putchar('\n');
+return (1);
+}
+_puts(_getenv(info, "OLDPWD="));
+_putchar('\n');
+chdirer_retu = chdir((direr = _getenv(info, "OLDPWD=")) ? direr : "/");
+}
+else
+chdirer_retu = chdir(info->argv[1]);
+if (chdirer_retu == -1)
+{
+print_error(info, "can't cd to ");
+_eputs(info->argv[1]);
+_eputchar('\n');
+}
+else
+{
+_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+_setenv(info, "PWD", getcwd(buff, 1024));
+}
+return (0);
 }
 
 /**
