6ac31aa83995816b7e2553c86638adda25fd9685
sweetpotatoX <67162997+sweetpotatoX@users.noreply.github.com>
2023-04-29 03:27:55 +0200

    Update history.c

--- history.c
+++ history.c
@@ -20,10 +20,6 @@ char *get_history_file(info_t *info)
 	buffer = malloc(sizeof(char) * (_strlen(directory) + _strlen(HIST_FILE) + 2));
 	if (!buffer)
 		return (NULL);
-
-	/* Set the buffer to an empty string, copy the directory path to the buffer,
-	 * concatenate the history file name to the end of the buffer.
-	 */
 	buffer[0] = '\0';
 	_strcpy(buffer, directory);
 	_strcat(buffer, "/");
@@ -76,6 +72,7 @@ int read_history(info_t *info)
 	ssize_t fd, rdlen, fsize = 0;
 	struct stat st;
 	char *buffer = NULL, *filn = get_history_file(info);
+	
 	if (!filn)
 		return (0);
 
