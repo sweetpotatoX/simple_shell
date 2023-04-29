cb230beebcac33339923cdf68f18851a7cad49e6
sweetpotatoX <67162997+sweetpotatoX@users.noreply.github.com>
2023-04-29 03:07:04 +0200

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,67 +1,77 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
+ * _strncpy - copies a string
+ * @dest: the destination string to be copied to
+ * @src: the source string
+ * @n: the amount of characters to be copied
+ *
+ * Return: the concatenated string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
-	char *s = dest;
+	int index1, index2;
+	char *str = dest;
 
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
+	index1 = 0;
+	while (src[index1] != '\0' && index1 < n - 1)
 	{
-		dest[i] = src[i];
-		i++;
+		dest[index1] = src[index1];
+		index1++;
 	}
-	if (i < n)
+	if (index1 < n)
 	{
-		j = i;
-		while (j < n)
+		index2 = index1;
+		while (index2 < n)
 		{
-			dest[j] = '\0';
-			j++;
+			dest[index2] = '\0';
+			index2++;
 		}
 	}
-	return (s);
+	return (str);
 }
 
 /**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
+ * _strncat - concatenates two strings
+ * @dest: the first string to concatenate
+ * @src: the second string to concatenate
+ * @n: the maximum number of bytes to be used from src
+ *
+ * Return: pointer to the concatenated string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-	char *s = dest;
+	int index1, index2;
+	char *str = dest;
 
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
+	/* Find the end of the first string */
+	index1 = 0;
+	while (dest[index1] != '\0')
+		index1++;
+
+	/* Append the second string to the first string */
+	index2 = 0;
+	while (src[index2] != '\0' && index2 < n)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest[index1] = src[index2];
+		index1++;
+		index2++;
 	}
-	if (j < n)
-		dest[i] = '\0';
-	return (s);
+
+	/* Add null terminator to the end of the concatenated string */
+	if (index2 < n)
+		dest[index1] = '\0';
+
+	return (str);
 }
 
 /**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
+ * _strchr - locates the first occurrence of a character in a string.
+ * @s: the string to be searched
+ * @c: the character to be located
+ *
+ * Return: a pointer to the first occurrence of the character c in the string s,
+ * or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
 {
